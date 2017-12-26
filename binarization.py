6caf667730938a95155231139cbedc15f3a66862
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2017-12-26 15:45:12 -0200

    Binarization params

--- binarization.py
+++ binarization.py
@@ -14,7 +14,10 @@ from matplotlib import pyplot as plt
 
 print "Executando"
 
-def callFromDirectory(p_folder,p_outfolder,createPath,bintype):
+trainedImage = []#cv::Image
+imagesToTrain = [] #[cv::image,cv::image]
+
+def callFromDirectory(p_folder,p_outfolder,createPath,bintype,bgTrain):
     '''
         Acessa um diretorio e seleciona todos os arquivos a serem processados
     '''
@@ -24,17 +27,24 @@ def callFromDirectory(p_folder,p_outfolder,createPath,bintype):
     print "PASTAS:", folders, "\n"
     for folder in folders:
         if(len(folder.split("."))<=1):
+            
+            #Reconhece e cria pastas
             if(createPath):
                 print "CRIANDO PASTA: ", folder[len(p_folder):]
                 os.system("mkdir "+p_outfolder+"/"+folder[len(p_folder):])
-            files = glob.glob(folder+"/*.*")
+            files = glob.glob(folder+"/*_left.*")
             print "Arquivos: ", files, "\n"
+
+            #Limpa treinamento do background
+            if(bgTrain):
+                clearBackgroundTrain()
+
             for image in files:
                 ext = image.split(".")
                 ext.reverse()
                 if(ext[0] in allowedExtensions):
                     if(createPath):
-                        imgPath = p_outfolder+image[len(folder):]
+                        imgPath = p_outfolder+folder[len(p_folder):]+image[len(folder):]
                     else:
                         imgPath = p_outfolder+"/"+image[len(p_folder)+1:].replace("/","-")
                     print "EXECUTANDO IMAGEM: ",image
@@ -42,7 +52,12 @@ def callFromDirectory(p_folder,p_outfolder,createPath,bintype):
                     #binarize(image,imgPath)
                     file_image = binarize(image,bintype)
                     file_image = resizeImage(file_image,2208,1242)
-                    saveImage(file_image,imgPath)
+                    if(bgTrain):
+                        if(trainBackground(file_image)):
+                            file_image = subImage(file_image,trainedBG())
+                            saveImage(file_image,imgPath)
+                    else:
+                        saveImage(file_image,imgPath)	
                     #run_crfasrnn(image,imgPath,gpudevice)
         else:
             print "NAO E DIRETORIO"
@@ -85,6 +100,48 @@ def binarize(input_img,p_type):
     return images[p_type]
 
 
+def trainedBG():
+	global trainedImage # = None #cv::Image
+	return trainedImage
+
+def clearBackgroundTrain():
+	'''
+		Limpa o treinamento do background
+	'''
+	global trainedImage # = None #cv::Image
+	global imagesToTrain # = [] #[cv::image,cv::image]
+	imagesToTrain = []
+	trainedImage = []
+
+def trainBackground(image):
+	'''
+		Treina background e retorna True ou False se estiver treinado
+	'''
+	ret = True
+	global trainedImage
+	global imagesToTrain
+	#print(trainedImage)
+
+	if(len(imagesToTrain) < 1 ):
+		imagesToTrain.append(image)
+		ret = False
+	elif(len(trainedImage) == 0):
+		#Train images
+		#trainedImage = subMultipleImages(imagesToTrain)
+		#trainedImage = subImage(imagesToTrain[0],imagesToTrain[1])
+		trainedImage = imagesToTrain[0]
+		ret = False
+	
+	return ret
+
+def subImage(img1, img2):
+	return cv2.bitwise_xor(img1,img2) #IMG1, IMG2, OUT
+
+
+def subMultipleImages(images):
+	return cv2.bitwise_xor(images[0],images[1],images[2]) #IMG1, IMG2, OUT
+
+
 def saveImage(image,p_output):
 	cv2.imwrite(p_output,image)
 
@@ -95,14 +152,19 @@ def main(argv):
    outputfolder = "out"
    createpath = False
    bintype = 1
+   bgTrain = False
    try:
-      opts, args = getopt.getopt(argv,'hi:o:b:f:p:c:',["ifile=","ofile=","bintype=","inputfolder=","outputfolder=","createPath="])
+      opts, args = getopt.getopt(argv,'hi:o:b:f:p:c:t:',["ifile=","ofile=","bintype=","inputfolder=","outputfolder=","createPath=","bgtrain="])
    except getopt.GetoptError:
-      print 'binarization.py -b <bintype> -i <inputfile> -o <outputfile> -f <inputfolder> -p <outputfolder> -c <createpath>'
+      print 'binarization.py -b <bintype> -i <inputfile> -o <outputfile> -f <inputfolder> -p <outputfolder> -c <createpath 1 or 0> -t <bgTrain 1 or 0>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'binarization.py -b <bintype> -i <inputfile> -o <outputfile> -f <inputfolder> -p <outputfolder> -c <createpath>'
+         print 'binarization.py -b <bintype> -i <inputfile> -o <outputfile> -f <inputfolder> -p <outputfolder> -c <createpath 1 or 0> -t <bgTrain 1 or 0>'
+         print '\n#<bintype>#'
+         print '0-> THRESH_BINARY'
+         print '1-> ADAPTIVE_THRESH_MEAN_C (DEFAULT)'
+         print '2-> ADAPTIVE_THRESH_GAUSSIAN_C'
          sys.exit()
       elif opt in ("-i", "ifile"):
          inputfile = arg
@@ -116,10 +178,12 @@ def main(argv):
          createpath = (arg == "1")
       elif opt in ("-b", "bintype"):
          bintype = int(arg)
+      elif opt in ("-t", "bgtrain"):
+         bgTrain = (arg == "1") 
 
    if(inputfolder != ""):
       print 'CALL FROM FOLDER',inputfolder
-      callFromDirectory(inputfolder,outputfolder,createpath,bintype)
+      callFromDirectory(inputfolder,outputfolder,createpath,bintype,bgTrain)
       return
 
    print 'Input file is "', inputfile
